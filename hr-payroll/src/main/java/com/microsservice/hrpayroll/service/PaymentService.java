2e84663943f259f4718fc4297cb4f8913676ece0
Lucas Teixeira <lucas.vieira@brmalls.com.br>
2021-02-22 13:10:41 -0300

    -feat: chamada de serviços com Feign Client

--- hr-payroll/src/main/java/com/microsservice/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/microsservice/hrpayroll/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.microsservice.hrpayroll.service;
 
+import com.microsservice.hrpayroll.client.WorkerFeignClient;
 import com.microsservice.hrpayroll.entity.Payment;
 import com.microsservice.hrpayroll.entity.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +14,12 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",
-                Worker.class,
-                uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
