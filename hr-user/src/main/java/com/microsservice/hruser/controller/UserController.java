18b87c7221dd97a18952b9b0930f9a55756830b1
Lucas <lucas.teixeira34@gmail.com>
2021-02-23 21:47:27 -0300

    UserRepository, UserController e Zuul Config

---
+++ hr-user/src/main/java/com/microsservice/hruser/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.microsservice.hruser.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microsservice.hruser.entity.User;
+import com.microsservice.hruser.repository.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+
+        User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+    
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findById(@RequestParam String email) {
+
+        User user = userRepository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+}
